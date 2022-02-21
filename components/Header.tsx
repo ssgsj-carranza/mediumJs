b587bf01eba8b9504fcb18101748508fd7cfb168
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-02-21 13:06:48 -0600

    added different items to the navbar and styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,13 +4,18 @@ import Link from "next/link";
 function Header() {
   return ( 
     <header>
-      <div>
+      <div className="flex items-center space-x-5">
         <Link href='/'>
             <img className='object-contain w-44 cursor-pointer'
                  src='https://cdn.logojoy.com/wp-content/uploads/2018/05/30164225/572.png' 
                  alt=''
             />
         </Link>
+        <div className='hidden md:inline-flex items-center space-x-5'>
+            <h3>About</h3>
+            <h3>Contact</h3>
+            <h3 className='text-white bg-green-600 rounded-full px-4 py-1 cursor-pointer'>Follow</h3>
+        </div>
       </div>
       
       <div>
