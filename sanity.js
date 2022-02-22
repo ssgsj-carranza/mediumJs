672ea2bcb19d577214b93d6925c118f1109e6234
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-02-22 10:43:14 -0600

    used sanity docs to pull in helper functions to fetch data, make queries and generating image

--- sanity.js
+++ sanity.js
@@ -6,3 +6,8 @@ export const config = {
     apiVersion: '2021-03-25',
     useCdn: process.env.NODE_ENV === 'production',
 };
+
+//set up the client for fetching data in the getProps page functions, make queries
+export const sanityClient = createClient(config);
+//set up helper function for generating Image Urls with only rhe asset reference data in your documents
+export const urlFor = (source) => createImageUrlBuilder(config).image(source);
