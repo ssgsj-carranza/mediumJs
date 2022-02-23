1a1ca8507776874db24f933ca5a37cf8a716d1b6
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-02-23 12:21:34 -0600

    finished styling posts, working on linking to other pages when posts are clicked

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,18 +35,18 @@ export default function Home({posts}:Props) {
         />  
       </div>
       {/* posts */}
-      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 lg:p-6'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6'>
         {posts.map((post) => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div className="group cursor-pointer border rounded-lg shadow-lg">
+            <div className="group cursor-pointer border rounded-lg shadow-lg overflow-hidden">
               <img className='h-60 w-full object-cover group-hover:scale-105 transition transform duration-200 ease-in-out'
                    src={urlFor(post.mainImage).url()!} 
                    alt="" 
               />
               <div className="flex justify-between p-5 bg-white">
                 <div>
-                  <p>{post.title}</p>
-                  <p>{post.description} by {post.author.name}</p>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">{post.description} <span className="font-bold">by</span> <span className="underline">{post.author.name}</span></p>
                 </div>
                 <img className='h-12 w-12 rounded-full'
                      src={urlFor(post.author.image).url()!} 
