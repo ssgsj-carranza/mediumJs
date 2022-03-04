d41d4291930e9b5ac62a211aa0f09a1b7ca57680
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-03-04 14:21:24 -0600

    pulled in and added some styling to author name, author image and post description

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -11,12 +11,20 @@ function Post({post}: Props) {
     return (
         <main>
             <Header />
-            <img className="w-fit mx-auto h-40 object-cover rounded-bl-3xl rounded-tr-3xl"
+            <img className="w-full h-40 px-2 object-cover rounded-bl-3xl rounded-tr-3xl"
                  src={urlFor(post.mainImage).url()!} 
                  alt="" 
             />
             <article className="max-w-3xl mx-auto p-5">
                 <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
+                <h2 className="text-xl font-light text-gray-500 mb-2">{post.description}</h2>
+                <div>
+                    <img className="h-10 w-10 rounded-full"
+                         src={urlFor(post.author.image).url()!} 
+                         alt="" 
+                    />
+                    <p className="font-extralight text-sm">Blog post by {post.author.name}</p>
+                </div>
             </article>
         </main>
 )}
