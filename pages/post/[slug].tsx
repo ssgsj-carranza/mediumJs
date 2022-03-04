4ab8f02ef44b330c05261ee603dfd1c4385ce534
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-03-04 13:49:38 -0600

    fixed typo, post page succesfully rendered, and post data pulled, tested using console lgo

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -3,7 +3,13 @@ import Header from '../../components/Header';
 import {sanityClient, urlFor} from '../../sanity';
 import { Post } from '../../typings';
 
-function Post() {
+interface Props {
+    post: Post;
+}
+
+function Post({post}: Props) {
+    console.log(post);
+
     return (
         <main>
             <Header />
@@ -14,7 +20,7 @@ export default Post;
 
 //pre-fetching all the routes, prepares page, gives back array of paths(slugs)
 export const getStaticPaths = async () => {
-    const query = `*[_type == 'post] {
+    const query = `*[_type == 'post'] {
         _id,
         slug {
             current
