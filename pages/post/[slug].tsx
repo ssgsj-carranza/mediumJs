bc730a25df3b299036b945a5773d446f6962bb1a
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-03-08 12:17:55 -0600

    pulled in blog summary from back end, workling on form for user to add and leave comments based on the post

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -27,17 +27,39 @@ function Post({post}: Props) {
                     <p className="font-extralight text-sm">Blog post by <span className='text-emerald-800'>{post.author.name}</span> - Published at {new Date(post._createdAt).toLocaleString()}</p>
                 </div>
                 
-                <div>
+                <div className='mt-10'>
                     <PortableText
                         className="" 
                         dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
                         projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
                         content={post.body}
-                        serializers={}
+                        serializers={
+                            {
+                                h1: (props: any) => (
+                                    <h1 className='text-2xl font-bold my-5' {...props}/>
+                                ),
+                                h2: (props: any) => (
+                                    <h1 className='text-2xl font-bold my-5' {...props}/>
+                                ),
+                                li: ({children}: any) => (
+                                    <li className='ml-4 list-disc'>{children}</li>
+                                ),
+                                link: ({href, children}: any) => (
+                                    <a href={href} className='text-blue-500 hover:underline'>{children}</a>
+                                ),
+                            }
+                        }
                     />
                 </div>
 
             </article>
+            <hr className='max-w-lg my-5 mx-auto border border-emerald-400'/>
+            <form>
+                <label>
+                    <span>Name</span>
+                    <input type='text' placeholder='Name here' />
+                </label>
+            </form>
         </main>
 )}
 
