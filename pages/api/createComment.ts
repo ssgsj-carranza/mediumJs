1ffca4e0c5a3f03cf46988d5b75363dc19d44033
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-03-09 10:05:49 -0600

    created comment file for end point and added config file to contain all necessary items

---
+++ pages/api/createComment.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+
+const config ={
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    useCdn: process.env.NODE_ENV ==='production',
+    token: process.env.SANITY_API_TOKEN,
+};
+
+
+export default function createComment(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.status(200).json({ name: 'John Doe' });
+}
