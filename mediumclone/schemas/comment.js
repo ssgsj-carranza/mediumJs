0fbeb4453955b71b624248a0e13e8f9f13409398
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-03-09 11:17:01 -0600

    created commnet folder inside schema, to create a comment table in the backend

---
+++ mediumclone/schemas/comment.js
@@ -0,0 +1,29 @@
+export default {
+    name: 'comment',
+    type: 'document',
+    title: 'Comment',
+    fields: [{
+        name: 'name',
+        type: 'string',
+    },
+    {
+        title: 'Approved',
+        name: 'approved',
+        type: 'boolean',
+        description: 'Comments wont show on this site without approval',
+    }, 
+    {
+        name: 'email',
+        type: 'string',
+    }, 
+    {
+        name: 'comment',
+        type: 'text',
+    }, 
+    {
+        name: 'post',
+        type: 'reference',
+        to: [{type: 'post'}],
+    },
+    ],
+};
